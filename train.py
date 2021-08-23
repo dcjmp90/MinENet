eadf2232cc94993765ad0ed2d633f8814880217c
Jon Perry <26749062+dcjmp90@users.noreply.github.com>
2021-08-23 17:10:36 -0500

    Update train.py

--- train.py
+++ train.py
@@ -1,41 +1,61 @@
 '''
 '''
-import argparse
-from MinENet import MinENet
-from Utils import Utils, Generator, OpenEDS, CityScape
-from keras.optimizers import Adam
-from keras.utils import multi_gpu_model
+import tensorflow as tf
+import numpy as np
+from tensorflow.keras.optimizers import Adam
+from tensorflow.keras.utils import multi_gpu_model
 from segmentation_models.losses import bce_dice_loss
 from segmentation_models.metrics import IOUScore
+from tensorflow.keras.callbacks import ModelCheckpoint, ReduceLROnPlateau
+from utils import TFData
+from model import MinENet
+from utils import openeds_labels
+from utils import args
+import os
 
 
-
-def _main(train, validate, util):
+def keras_main():
     '''
     '''
-    G_train = Generator(train, util.batch_size)
-    G_validate = Generator(validate, util.batch_size)
-    model = MinENet(util)
-    model = model.createModel()
-    metric = IOUScore()
-    print(model.summary())
-    with open(util.save_weights_path+'model_summary.txt','w') as f:
-        model.summary(print_fn=lambda line: f.write(line + '\n'))
-    model = multi_gpu_model(model, gpus=2)
-    model.compile( loss = bce_dice_loss, optimizer = Adam(), metrics=[metric])
-    for epoch in range(util.epochs):
-        model.fit_generator(  G_train.generate(util)
-                            , util.steps_per_epoch
-                            , validation_data=G_validate.generate(util)
-                            , validation_steps=util.validation_steps
-                            , epochs = 1
-                            #, use_multiprocessing = True
-                            )
-        model.save_weights(util.save_weights_path + 'weights.'+str(epoch))
-        model.save(util.save_weights_path+'model.'+str(epoch))
+    mirrored_strat = tf.distribute.MirroredStrategy()
+    with mirrored_strat.scope():
+        train = TFData(args.TRAIN_IMAGES, args.TRAIN_LABELS, args, openeds_labels)
+        validate = TFData(args.VAL_IMAGES, args.VAL_LABELS, args, openeds_labels)
+        validation_data = validate.prepare().repeat(args.EPOCHS)
+        metric = IOUScore(class_indexes=[1,2,3])
+        os.makedirs(args.SAVE_PATH+args.MODEL_NAME+'imgs/', exist_ok=True)
+        os.makedirs(args.SAVE_PATH+args.MODEL_NAME+'hdf5/', exist_ok=True)
+        save_path = args.SAVE_PATH+args.MODEL_NAME+'hdf5/epoch_{epoch:03d}_val-loss_{val_loss:0.3f}_val-iou_{val_iou_score:0.3f}.hdf5'
+        checkpoint = ModelCheckpoint(save_path, 
+                                     monitor='val_loss',
+                                     verbose=1,
+                                     save_best_only=False,
+                                     save_weights_only=False,
+                                     mode='auto',
+                                     save_freq='epoch',
+                                    )
+        learn_rate_on_plat = ReduceLROnPlateau(monitor='val_loss', verbose=1, min_lr = 1e-5, patience = 5)
+        model = MinENet(args)
+        model = model.create_model()
+        print("model created")
+        model.compile(loss=bce_dice_loss,
+                      optimizer=Adam(lr=args.LEARN_RATE),
+                      metrics=[metric],
+                     )
+        print("model compiled")
+        model.fit(train.prepare().repeat(args.EPOCHS), 
+                  steps_per_epoch=len(train)//args.BATCH_SIZE, 
+                  validation_data=validation_data, 
+                  validation_steps=len(validate)//args.BATCH_SIZE, 
+                  epochs = args.EPOCHS, 
+                  shuffle=False, 
+                  use_multiprocessing = True, 
+                  workers = 36, 
+                  callbacks = [ checkpoint, 
+                                learn_rate_on_plat,
+                              ],
+                 )
 
 
 if __name__ == '__main__':
-    cityscape = CityScape()
-    train_data, validate_data = cityscape.create_dataset()
-    _main(train_data, validate_data, cityscape)
+    keras_main()
